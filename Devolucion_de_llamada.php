b7c1b02ed8fe1dc12e1c26a5dde1a9f3809f9e65
Daniel Jared Santander Leon <102763278+DanielJaree@users.noreply.github.com>
2023-07-25 08:47:46 -0500

    Update Devolucion_de_llamada.php

--- Devolucion_de_llamada.php
+++ Devolucion_de_llamada.php
@@ -1,11 +1,26 @@
 <?php
-$int = 122;
-$min = 1;
-$max = 200;
-
-if (filter_var($int, FILTER_VALIDATE_INT, array("options" => array("min_range"=>$min, "max_range"=>$max))) === false) {
-  echo("Variable value is not within the legal range");
-} else {
-  echo("Variable value is within the legal range");
+// Definimos una función que recibe un número y un callback
+function operar($numero, $callback) {
+    // Aplicamos el callback al número y retornamos el resultado
+    return $callback($numero);
 }
+
+// Definimos una función de callback que suma 10 al número recibido
+function sumarDiez($numero) {
+    return $numero + 10;
+}
+
+// Definimos otra función de callback que resta 5 al número recibido
+function restarCinco($numero) {
+    return $numero - 5;
+}
+
+// Llamamos a la función operar con diferentes callbacks
+$numero = 20;
+$resultadoSuma = operar($numero, "sumarDiez");
+$resultadoResta = operar($numero, "restarCinco");
+
+// Mostramos los resultados
+echo "Resultado de la suma: " . $resultadoSuma; // Output: Resultado de la suma: 30
+echo "Resultado de la resta: " . $resultadoResta; // Output: Resultado de la resta: 15
 ?>
